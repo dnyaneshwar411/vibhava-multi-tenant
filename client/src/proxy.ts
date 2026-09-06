15d78e9653125a5b9516a9128599d6746f25cb33
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2026-09-06 09:36:53 +0530

    fix: updated client middleware to handle company pages

--- client/src/proxy.ts
+++ client/src/proxy.ts
@@ -4,6 +4,8 @@ import { cookies } from 'next/headers';
 
 const unAuthTenantRoutes = ["/login"]
 
+const allowedTenantPaths = ["/", "/about", "/contact", "/privacy-policy", "/terms-conditions"]
+
 function extractSubdomain(request: NextRequest): string | null {
   const url = request.url;
   const host = request.headers.get('host') || '';
@@ -44,6 +46,9 @@ export default async function middleware(request: NextRequest) {
   const access = cookiesList.get("access")?.value;
 
   if (subdomain) {
+    console.log(pathname)
+    if (allowedTenantPaths.includes(pathname)) return NextResponse.next()
+
     const path = pathname.split("/")
     if (path.includes('admin')) {
       return NextResponse.redirect(new URL('/', request.url));
